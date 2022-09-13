c524df5486ce55d5cffee2976480fd0caddd8eb0
Camille Linan <camille.linan@hotmail.com>
2022-09-13 22:58:52 +0200

    corrections

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from './components/Routes/index.routes';
+
+const App = () => {
+
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
