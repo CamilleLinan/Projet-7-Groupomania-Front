5cee099f004084bb1cf2527147e48060343b62a5
Camille Linan <camille.linan@hotmail.com>
2022-09-14 02:41:31 +0200

    SignUp OK

--- src/components/Log/SignUpForm.jsx
+++ src/components/Log/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState }  from "react";
 import { useForm } from 'react-hook-form';
-import axios from 'axios'; 
+import axios from 'axios';
 
 const SignUpForm = () => {
     const [ formSubmit, setFormSubmit ] = useState(false);
@@ -8,7 +8,9 @@ const SignUpForm = () => {
     const [ lastname, setLastname ] = useState('');
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
-    
+    const [ errorEmail, setErrorEmail ] = useState('');
+    const [ errorServer, setErrorServer ] = useState('');
+
     const { register, formState: { errors }, handleSubmit } = useForm();
     const onSubmit = () => 
         
@@ -26,7 +28,14 @@ const SignUpForm = () => {
                 console.log(res);
                 setFormSubmit(true);
             })
-            .catch((err) => console.log(err));
+            .catch((error) => {
+                if (error.response.status === 400) {
+                    console.log(error.response);
+                    setErrorEmail({ ...errorEmail, message: 'Cette adresse email est déjà utilisée' });
+                } else {
+                    setErrorServer({ ...errorServer, message: 'Une erreur interne est survenue. Merci de revenir plus tard.' })
+                }
+            });
 
     return (
     <>
@@ -45,8 +54,8 @@ const SignUpForm = () => {
                 id="firstname"
                 className="form_input"
                 {...register('firstname', { required: true, minLength: 2, maxLength: 20, pattern: /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/i })}
-                value={firstname}
                 onChange={(e) => setFirstname(e.target.value)}
+                value={firstname}
             />
             <div className="error bold">{errors.firstname && "Veuillez renseigner votre prénom sans chiffre ni caractère spécial"}</div>
             <br/>
@@ -58,8 +67,8 @@ const SignUpForm = () => {
                 id="lastname"
                 className="form_input"
                 {...register('lastname', { required: true, minLength: 2, maxLength: 20, pattern: /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/i })}
-                value={lastname}
                 onChange={(e) => setLastname(e.target.value)}
+                value={lastname}
             />
             <div className="error bold">{errors.lastname && "Veuillez renseigner votre nom sans chiffre ni caractère spécial"}</div>
             <br/>
@@ -70,11 +79,11 @@ const SignUpForm = () => {
                 name="email"
                 id="email"
                 className="form_input"
-                {...register('email', { required: true, pattern: /^[a-z0-9 .-]+@[a-z .-]+\.[a-z]{2,3}$/ })}
-                value={email}
+                {...register('email', { required: true, pattern: /^[A-Z0-9._-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i })}
                 onChange={(e) => setEmail(e.target.value)}
+                value={email}
             />
-            <div className="error bold">{errors.email && `Veuillez renseigner une adresse mail type : exemple@mail.com`}</div>
+            <div className="error bold">{errors.email && `Email incorrecte`}{errorEmail.message}</div>
             <br/>
             <label htmlFor="password" className="form_label">Mot de passe</label>
             <br/>
@@ -84,11 +93,12 @@ const SignUpForm = () => {
                 id="password"
                 className="form_input"
                 {...register('password', { required: true, minLength: 4, maxLength: 30, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,}$/ })}
-                value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                value={password}
             />
             <div className="error bold">{errors.password && `Le mot de passe doit contenir entre 4 et 30 caractères, au moins une majuscule et une minuscule, et au moins un chiffre`}</div>
             <br/>
+            <span className="error bold">{errorServer.message}</span>
             <button type="submit" className="btn btn_form">Créer un compte</button>
         </form>
         </>
